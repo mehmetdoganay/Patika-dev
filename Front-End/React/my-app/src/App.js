50edabe76825a0ff36c41e4158b53031f7231d4d
Mehmet Doganay <mehmetdoganay@Mehmets-MacBook-Air.local>
2022-06-18 01:24:20 +0300

    döngüler ve key kullanımı

--- Front-End/React/my-app/src/App.js
+++ Front-End/React/my-app/src/App.js
@@ -2,12 +2,40 @@ import './App.css';
 import Header from './components/Header'
 import User from './components/User';
 
+const friends = [
+  {
+    id: 1,
+    name: 'Burak',
+  },
+  {
+    id: 2,
+    name: 'Mehmet',
+  },
+  {
+    id: 3,
+    name: 'Ali',
+  },
+  {
+    id: 4,
+    name: 'Mahmut',
+  },
+  {
+    id: 5,
+    name: 'Osman',
+  },
+];
 
 function App() {
   return (
     <div>
       <Header/>
-      <User name="Mehmet" surname = "Doğanay" age={23} isLoggedIn = {true} />
+      <User 
+        name="Mehmet" 
+        surname = "Doğanay" 
+        age={23} 
+        isLoggedIn = {true} 
+        friends = {friends}
+        />
     </div>
   )
 }
--- Front-End/React/my-app/src/components/User.js
+++ Front-End/React/my-app/src/components/User.js
@@ -1,11 +1,18 @@
-function User({name, surname, isLoggedIn, age}) {
+function User({name, surname, isLoggedIn, age, friends}) {
     return (
-        <h1>
+        <>
+            <h1>
+                {
+                    isLoggedIn ? `${name} ${surname} ${age}` : `Giriş Yapılmadı`
+                }
+            </h1>
+
             {
-                isLoggedIn ? `${name} ${surname} ${age}` : `Giriş Yapılmadı`
+                friends.map((friend) => (
+                <div key={friends.id}>{friend.id} -{friend.name}</div>))
             }
-        </h1>
-    )
+        </>
+    );
 }
 
 export default User
